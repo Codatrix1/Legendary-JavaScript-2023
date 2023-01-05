0c6802529ecc33ad05dcdbdb2e939c42011b9260
Codatrix <ankur.codatrix@gmail.com>
2023-01-05 11:26:00 +0530

    Strings - Template literals - if/else - type conversion and coercion

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -243,6 +243,8 @@ console.log(currentYear - 1990 > currentYear - 1992); // true
 // LECTURE - 15: OPERATOR PRECEDENCE
 //----------------------------------------------------------
 
+/*
+
 const currentYear = 2077;
 
 const agePeter = currentYear - 1990;
@@ -257,3 +259,213 @@ console.log(x, y); // 20 20
 
 const averageAge = (ageCarrie + agePeter) / 2;
 console.log(ageCarrie, agePeter, averageAge); // 66 87 76.5
+
+*/
+
+//----------------------------------------------------------
+// LECTURE - 16: CODING CHALLENGE # 1
+//----------------------------------------------------------
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+
+
+Test data:
+  - Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+  - Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(bmiMark, bmiJohn);
+
+const markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI); // true
+
+*/
+
+//----------------------------------------------------------
+// LECTURE - 17: STRINGS and TEMPLATE LITERALS
+//----------------------------------------------------------
+
+/*
+
+const firstName = "Peter";
+const occupation = "programmer";
+const birthYear = 1989;
+const year = 2037;
+
+const peterDetails =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + occupation;
+
+console.log(peterDetails);
+
+const peterNewDetails = `I'm ${firstName}, a ${
+  year - birthYear
+} year old ${occupation}.`;
+
+console.log(peterNewDetails);
+
+console.log(`This is a regular string....`);
+
+//---------------------
+// MULTILINE STRINGS
+//--------------------
+console.log(
+  "String with \n\
+multiple \n\
+lines"
+);
+
+console.log(`Template literal:
+multiline
+string`);
+
+*/
+
+//----------------------------------------------------------
+// LECTURE - 18: TAKING DECISIONS: if / else STATEMENTS
+//----------------------------------------------------------
+
+/*
+
+const age = 15;
+
+if (age >= 18) {
+  console.log(`Carrie can have her driving license`);
+} else {
+  const yearsLeft = 18 - age;
+  console.log(
+    `Carrie will have to wait for another ${yearsLeft} to start driving`
+  );
+}
+
+const birthYear = 2012;
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+
+*/
+
+//----------------------------------------------------------
+// LECTURE - 19: CODING CHALLENGE # 2
+//----------------------------------------------------------
+
+/*
+
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiJohn = massJohn / (heightJohn * heightJohn);
+
+console.log(bmiMark, bmiJohn);
+
+const markHigherBMI = bmiMark > bmiJohn;
+console.log(markHigherBMI); // true
+
+if (bmiMark > bmiJohn) {
+  console.log(`Mark's BMI ${bmiMark} is greater than John's ${bmiJohn}`);
+} else {
+  console.log(`John's BMI ${bmiJohn} is greater`);
+}
+
+*/
+
+//----------------------------------------------------------
+// LECTURE - 20: Type CONVERSION And Type COERCION
+//----------------------------------------------------------
+
+/*
+
+//----------------------
+// 🔴 Type Conversion - When we manually convert one type to another
+//----------------------
+
+const inputYear = "1991";
+
+console.log(Number(inputYear), inputYear); // 1991 '1991'
+
+console.log(Number(inputYear) + 18); // 2009
+console.log(Number("Hello")); // NaN
+
+console.log(typeof NaN); // number
+
+console.log(23, String(23)); // 23 '23'
+
+//------------------
+// 🔴 Type Coercion
+//------------------
+
+// When JavaScript converts automatically converts types behind the scenes for us. It happens whenever an operator is dealing with two values that have different types, so JavaScript will convert one value to match with the other value, so that in the end the operation can be successfully executed.
+
+//  "+" operator converts NUMBERS to STRINGS
+console.log("I am " + 23 + " years old"); // I am 23 years old
+console.log("I am " + "23" + " years old"); // I am 23 years old
+
+console.log("10" + "5" + 3); // 1053
+console.log(typeof ("10" + "5" + 3)); // string
+
+// "-" operator coverts STRINGS to NUMBERS
+console.log("10" - "5" - 3); // 2
+console.log(typeof ("10" - "5" - 3)); // number
+
+// Also
+console.log("22" / "2"); // 11
+console.log(typeof ("22" / "2")); // number
+
+//---------
+// EXAMPLES
+//---------
+let n = "1" + 1;
+n = n - 1;
+console.log(n, typeof n); // 10 'number'
+
+console.log(2 + 3 + 4 + "5"); // 95
+console.log(typeof (2 + 3 + 4 + "5")); // string
+
+console.log(typeof ("10" - "4" - "3" - 2 + "00")); // 100
+console.log(typeof ("10" - "4" - "3" - 2 + "00")); // string
+
+*/

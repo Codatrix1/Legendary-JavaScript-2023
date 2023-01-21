c7c9219f49a8ecb356f615dbd4780f1923b2a9d6
Codatrix <ankur.codatrix@gmail.com>
2023-01-21 10:30:02 +0530

    Nullish Coalescing Operator - ?? - TURNS 0 and '' to TRUTHY - and NOT null or undefined

---
+++ 05-Data-Structures-Operators-And-Strings/06-Nullish-Coalescing-Operator.js
@@ -0,0 +1,76 @@
+"use strict";
+
+//-----------------------------------------------
+// LECTURE 108: NULLISH COALESCING Operator (??)
+//------------------------------------------------
+
+//.....................................
+// 🔴 OUR DATA
+//.....................................
+
+const restaurant = {
+  nameOfRestaurant: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIdx, mainIdx) {
+    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+
+  // Destructuring inside the args itself: with default values
+  orderDelivery: function ({
+    stIndex = 0,
+    mnIndex = 1,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[stIndex]} and ${this.mainMenu[mnIndex]} will be delivered at ${address} by ${time} hours`
+    );
+  },
+
+  // METHOD: Function to order pasta using the SPREAD Operator
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your order of pasta with ingrediants ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  // METHOD: Function to order pizza using the REST Operator
+  orderPizza: function (mainIngrediant, ...otherIngrediants) {
+    console.log(mainIngrediant, otherIngrediants);
+  },
+};
+
+//--------------------------------------------------
+// THE "0" Problem: Solved with Nullish Coalescing
+// 🔥 ES2020
+//--------------------------------------------------
+
+// defined already
+restaurant.numGuests = 0;
+
+// with OR
+const guests = restaurant.numGuests || 23;
+console.log(guests); // ❗ 23
+
+// Nullish: undefined and null (NOT 0 or "")
+const correctGuests = restaurant.numGuests ?? 45;
+console.log(correctGuests); // 0
